bf53668adea9c262cb0d6505cc12567dd7b2d80b
Rodney Ossai <ossai2007@gmail.com>
2017-05-16 15:58:25 +0100

    Put in Code that does the Logic

--- aritGeo.js
+++ aritGeo.js
@@ -1,25 +1,30 @@
 let aritGeo = (a) => {
-  function checkAP(){
-      //Logic here
+  let result;
+  function checkAP(a){
+      let commonDifference =a[1] - a[0];
+      for(let i = 0;i<a.length-1;i++){
+            if( (a[i+1] - a[i]) != commonDifference){
+              return false;
+            }
+      }
+      return true;
   }
-  function checkGP(){
-       //Logic here
+  function checkGP(a){
+       let commonRatio = a[1] / a[0];
+       for(let i = 0 ; i<a.length-1;i++){
+          if ( a[i+1] / a[i] != commonRatio){
+            return false;
+          }
+       }
+       return true;
   }
-  if (checkAP===true){
-          //Logic here
-  }else if(checkGP===true){
-      //Logic here
-  }else if (!(checkAP==true) && !(checkGP==true){
-      //Logic here
+  if (checkAP(a)===true){
+          result = "Arithmetic";
+  }else if(checkGP(a)===true){
+          result = "Geometric";
   }
-  else{
-
-    if(arr.length = 0){
-      //Logic here
-    }
-    if(arr.length<2){
-      //Logic here
-    }
+  else if (!(checkAP==true) && !(checkGP==true){
+      result = -1
   }
-          
+ return result;         
 };
