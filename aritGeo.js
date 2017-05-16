44fa8360256b337c0af55888cb4993200b8999c8
Rodney Ossai <ossai2007@gmail.com>
2017-05-16 14:46:14 +0100

    Code skeleton

---
+++ aritGeo.js
@@ -0,0 +1,25 @@
+let aritGeo = (a) => {
+  function checkAP(){
+      //Logic here
+  }
+  function checkGP(){
+       //Logic here
+  }
+  if (checkAP===true){
+          //Logic here
+  }else if(checkGP===true){
+      //Logic here
+  }else if (!(checkAP==true) && !(checkGP==true){
+      //Logic here
+  }
+  else{
+
+    if(arr.length = 0){
+      //Logic here
+    }
+    if(arr.length<2){
+      //Logic here
+    }
+  }
+          
+};
