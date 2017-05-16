ce9bf25f3b3df9b94c2e8376e2960d9c1f4703c7
Rodney Ossai <ossai2007@gmail.com>
2017-05-16 16:55:44 +0100

    Complete refactored code with comments

--- aritGeo.js
+++ aritGeo.js
@@ -1,6 +1,6 @@
 let aritGeo = (a) => {
-  let result;
-  function checkAP(a){
+  let result; //Declare a variable to hold the answer for each logic
+  function checkAP(a){  //Checks if the input is Arithmetic progression and returns true.
       let commonDifference =a[1] - a[0];
       for(let i = 0;i<a.length-1;i++){
             if( (a[i+1] - a[i]) != commonDifference){
@@ -9,7 +9,7 @@ let aritGeo = (a) => {
       }
       return true;
   }
-  function checkGP(a){
+  function checkGP(a){ //Checks if the input is Geometric progression and returns true.
        let commonRatio = a[1] / a[0];
        for(let i = 0 ; i<a.length-1;i++){
           if ( a[i+1] / a[i] != commonRatio){
@@ -18,13 +18,17 @@ let aritGeo = (a) => {
        }
        return true;
   }
-  if (checkAP(a)===true){
+  
+  if (typeof a[0] === 'undefined'){ // If position 0 in the array is undefined it means that the array ois empty so sets tesult to 0
+      result = 0;
+    }
+  else if (checkAP(a)===true){ // Sets result to "Arithmetic" if checkAP() function is True
           result = "Arithmetic";
-  }else if(checkGP(a)===true){
+  }else if(checkGP(a)===true){ // Sets result to "Geometric" if checkGP() function is True
           result = "Geometric";
+  }else if (!((checkAP(a)===true) && (checkGP(a)===true))){ // Sets result to -1 if checkGP() and check AP is not True.
+      result = -1;
   }
-  else if (!(checkAP==true) && !(checkGP==true){
-      result = -1
-  }
- return result;         
+  
+ return result;     // Returns result.    
 };
